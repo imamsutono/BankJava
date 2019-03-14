deef18d89b00630cb6da113b1849510236bd162f
imamsutono <imamssutono@gmail.com>
2019-03-15 00:03:05 +0700

    Add Nasabah class

---
+++ Nasabah.java
@@ -0,0 +1,18 @@
+/**
+ * Nasabah
+ */
+public class Nasabah extends Bank {
+
+  String[][] dataNasabah = {
+    {"123", "imam", "991"},
+    {"1234", "wildan", "6914239"}
+  };
+
+  public Nasabah() {
+    super();
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
